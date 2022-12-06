381c21d75e50c99d631553ef367e7e5afeb700d4
DungHoang <hoangdung.200298@gmail.com>
2022-12-06 17:35:21 +0700

    cập nhật dữ liệu mẫu

--- scrips/Init_Database.sql
+++ scrips/Init_Database.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS `Control`(
     CONSTRAINT PK_Control PRIMARY KEY (ControlKey,ControlCode)
 );
 CREATE TABLE IF NOT EXISTS `Image` (
-  Id int(11) NOT NULL AUTO_INCREMENT,
+  Id int NOT NULL AUTO_INCREMENT,
   Filename varchar(100) NOT NULL,
   PRIMARY KEY (Id)
 );
@@ -25,6 +25,7 @@ CREATE TABLE IF NOT EXISTS `User`(
     Gender TINYINT,
     Email VARCHAR(100),
     Phone CHAR(15),
+    IsActive TINYINT DEFAULT 0,
     CreatedDateTime DATETIME DEFAULT CURRENT_TIMESTAMP(),
     CONSTRAINT UC_User UNIQUE (Username)
 );
@@ -95,49 +96,111 @@ INSERT INTO `control`(`ControlKey`,`ControlCode`,`ControlName`) VALUES ('Categor
 INSERT INTO `control`(`ControlKey`,`ControlCode`,`ControlName`) VALUES ('Category',9,'Giày bóng đá');
 INSERT INTO `control`(`ControlKey`,`ControlCode`,`ControlName`) VALUES ('Category',10,'Khác');
 
-INSERT INTO `user`(`Usertype`,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`)
-VALUES (0, 'sa', '123', 'System', 'Admin', '1999-01-20', 0, 'sys.bk@gmail.com', '037 492 1052');
-INSERT INTO `user`(`Usertype`,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`)
-VALUES (1, '1', '1', 'Luis', 'Nani', '1989-11-26', 0, 'nani@gmail.com', '032 132 4331');
-INSERT INTO `user`(`Usertype`,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`)
-VALUES (0, '2', '2', 'Reece', 'James', '1999-02-20', 0, 'james@gmail.com', '032 434 1552');
+INSERT INTO `user`(`Usertype`,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`,`IsActive`)
+VALUES (0, 'sa', '123', 'System', 'Admin', '1999-01-20', 0, 'sys.bk@gmail.com', '037 492 1052',1);
+INSERT INTO `user`(`Usertype`,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`,`IsActive`)
+VALUES (1, '1', '1', 'Luis', 'Nani', '1989-11-26', 0, 'nani@gmail.com', '032 132 4331',1);
+INSERT INTO user(Usertype,`Username`,`Password`,`FirstName`,`LastName`,`Birthday`,`Gender`,`Email`,`Phone`,`IsActive`)
+VALUES (0, '2', '2', 'Reece', 'James', '1999-02-20', 0, 'james@gmail.com', '032 434 1552',1);
 
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo AC Milan sân nhà', 1, '2022-2023', '2','AC1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo AC Milan sân khách', 2, '2022-2023', '2','AC2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Áo Arsenal sân nhà', 1, '2022-2023', '1','arsenal1.jpg','arsenal1.jpg','arsenal1.jpg','arsenal1.jpg',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo Barca sân nhà', 1, '2022-2023', '1','barca1.jpg','barca1.jpg','barca1.jpg','barca1.jpg',120000, 80000, 1, 20, 'Mô tả');
-
-INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo Chelsea sân nhà', 1, '2022-2023', '3','chelsea1.jpg','chelsea1.jpg','chelsea1.jpg','chelsea1.jpg',120000, 80000, 1, 20, 'Mô tả');
-
-INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo Real sân nhà', 1, '2022-2023', '3','real1.jpg','real1.jpg','real1.jpg','real1.jpg',120000, 80000, 1, 20, 'Mô tả');
-
-INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Áo Arsenal sân khách', 2, '2022-2023', '1','arsenal2.jpg','arsenal2.jpg','arsenal2.jpg','arsenal2.jpg',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Barca sân nhà', 1, '2022-2023', '1','barca1.jpg','barca1.jpg','barca1.jpg','barca1.jpg',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Áo Barca sân khách', 2, '2022-2023', '1','barca2.jpg','barca2.jpg','barca2.jpg','barca2.jpg',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Bayern sân nhà', 1, '2022-2023', '1','bayern1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Bayern sân khách', 2, '2022-2023', '1','bayern2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Chelsea sân nhà', 1, '2022-2023', '3','chelsea1.jpg','chelsea1.jpg','chelsea1.jpg','chelsea1.jpg',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Áo Chelsea sân khách', 1, '2022-2023', '3','chelsea2.jpg','chelsea2.jpg','chelsea2.jpg','chelsea2.jpg',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Dortmund sân nhà', 1, '2022-2023', '1','dortmund1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Dortmund sân khách', 2, '2022-2023', '1','dortmund2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Juventus sân nhà', 1, '2022-2023', '1','juventus1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Juventus sân khách', 2, '2022-2023', '1','juventus2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Liverpool sân nhà', 1, '2022-2023', '2','liver1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Liverpool sân khách', 2, '2022-2023', '2','liver2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo MU sân nhà', 1, '2022-2023', '2','MU1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo MU sân khách', 2, '2022-2023', '2','MU2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo MC sân nhà', 1, '2022-2023', '2','MC1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo MC sân khách', 2, '2022-2023', '2','MC2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo PSG sân nhà', 1, '2022-2023', '3','psg1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo PSG sân khách', 2, '2022-2023', '3','psg2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Real sân nhà', 1, '2022-2023', '3','real1.jpg','real1.jpg','real1.jpg','real1.jpg',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Áo Real sân khách', 2, '2022-2023', '3','real2.jpg','real2.jpg','real2.jpg','real2.jpg',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo Brazil sân nhà', 5, 'WC 2022', '2','brazil1.jpg','brazil1.jpg','brazil1.jpg','brazil1.jpg',120000, 80000, 1, 20, 'Mô tả');
+VALUES ('Áo Argentina sân nhà', 5, 'WC 2022', '2','arg1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo BĐN sân nhà', 5, 'WC 2022', '2','portugal1.jpg','portugal1.jpg','portugal1.jpg','portugal1.jpg',120000, 80000, 1, 20, 'Mô tả');
+VALUES ('Áo Argentina sân khách', 6, 'WC 2022', '2','arg2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo Brazil sân khách', 6, 'WC 2022', '2','brazil2.jpg','brazil2.jpg','brazil2.jpg','brazil2.jpg',120000, 80000, 1, 20, 'Mô tả');
+VALUES ('Áo Brazil sân nhà', 5, 'WC 2022', '2','brazil1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
-VALUES ('Áo BĐN sân khách', 6, 'WC 2022', '2','portugal2.jpg','portugal2.jpg','portugal2.jpg','portugal2.jpg',120000, 80000, 1, 20, 'Mô tả');
+VALUES ('Áo Brazil sân khách', 6, 'WC 2022', '2','brazil2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo BĐN sân nhà', 5, 'WC 2022', '2','portugal1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo BĐN sân khách', 6, 'WC 2022', '2','portugal2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Pháp sân nhà', 5, 'WC 2022', '2','france1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Đức sân nhà', 5, 'WC 2022', '2','germany1.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Áo Đức sân khách', 6, 'WC 2022', '2','germany2.jpg','','','',120000, 80000, 1, 20, 'Mô tả');
 
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
@@ -149,14 +212,19 @@ VALUES ('Giày Copa xanh dương', 9, '2022', '1','shoe2.jpg','shoe2.jpg','shoe1
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Giày Copae hồng', 9, '2022', '1','shoe3.jpg','shoe3.jpg','shoe3.jpg','shoe3.jpg',390000, 270000, 1, 20, 'Mô tả');
 
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Giày Copa đỏ', 9, '2022', '1','shoe4.jpg','','','',380000, 260000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Giày Copa đen', 9, '2022', '1','shoe5.jpg','','','',380000, 260000, 1, 20, 'Mô tả');
+
+INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
+VALUES ('Giày Copae trắng', 9, '2022', '1','shoe6.jpg','','','',380000, 260000, 1, 20, 'Mô tả');
+
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Bóng futsal vàng xanh', 10, '2022', '1','ball1.jpg','ball1.jpg','ball1.jpg','ball1.jpg',250000, 170000, 1, 10, 'Mô tả');
 
 INSERT INTO `product`(`ProductName`,`CategoryId`,`Season`,`Brand`,`Image1`,`Image2`,`Image3`,`Image4`,`SalesPrice`,`PurchasePrice`,`AllowSales`,`Inventory`,`Description`)
 VALUES ('Bóng futsal trắng đỏ', 10, '2022', '1','ball2.jpg','ball2.jpg','ball2.jpg','ball2.jpg',250000, 170000, 1, 10, 'Mô tả');
-
-
-
-
 
